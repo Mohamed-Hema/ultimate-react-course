8007e55389a17cf9a59edaae8514f0f453536052
Mohamed Hema <mohamedhema2017@gmail.com>
2023-12-25 15:08:19 +0200

    start quiz app

--- 10-react-quiz/starter/react-quiz/src/App.js
+++ 10-react-quiz/starter/react-quiz/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+
+      <main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </main>
     </div>
   );
 }
-
-export default App;
