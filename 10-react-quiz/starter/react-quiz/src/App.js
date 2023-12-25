37b151f35f97ea44f06f22a0233027fd6367f5d5
Mohamed Hema <mohamedhema2017@gmail.com>
2023-12-25 16:44:41 +0200

    updates to quiz app

--- 10-react-quiz/starter/react-quiz/src/App.js
+++ 10-react-quiz/starter/react-quiz/src/App.js
@@ -1,14 +1,58 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import Main from "./main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import { Question } from "./Question";
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
 
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      throw new Error("Action is unknown");
+  }
+}
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch(`http://localhost:9000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
 
-      <main className="main">
-        <p>1/15</p>
-        <p>Question?</p>
-      </main>
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
+        )}
+        {status === "active" && <Question />}
+      </Main>
     </div>
   );
 }
---
+++ 10-react-quiz/starter/react-quiz/src/StartScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const StartScreen = ({ numQuestions, dispatch }) => {
+  return (
+    <div className="start">
+      <h2>Welcome to React Quiz</h2>
+      <h3>{numQuestions} questions to test your React Mastery</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's Start
+      </button>
+    </div>
+  );
+};
+
+export default StartScreen;
