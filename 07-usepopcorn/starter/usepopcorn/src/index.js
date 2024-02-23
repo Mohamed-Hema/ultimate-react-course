f317158b9c042eb2a6d848542ca550138617e202
Mohamed Hema <mohamedhema2017@gmail.com>
2024-02-23 19:57:35 +0200

    lecture 119 so far

--- 07-usepopcorn/starter/usepopcorn/src/index.js
+++ 07-usepopcorn/starter/usepopcorn/src/index.js
@@ -8,6 +8,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" />
   </React.StrictMode>
 );
