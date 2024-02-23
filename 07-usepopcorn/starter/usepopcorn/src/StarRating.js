06a1f82290c329fb83fbbdd70c7b5b83a2cdca7d
Mohamed Hema <mohamedhema2017@gmail.com>
2024-02-23 19:43:21 +0200

    lecture 117 ✅

--- 07-usepopcorn/starter/usepopcorn/src/StarRating.js
+++ 07-usepopcorn/starter/usepopcorn/src/StarRating.js
@@ -14,7 +14,9 @@ const textStyle = {
 };
 
 export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
+
   function handleRating(rating) {
     setRating(rating);
   }
@@ -26,12 +28,14 @@ export default function StarRating({ maxRating = 5 }) {
             <Star
               key={i}
               onRate={() => handleRating(i + 1)}
-              full={rating >= i + 1}
+              full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
             />
           </span>
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -41,9 +45,15 @@ const starStyle = {
   display: "block",
   cursor: "pointer",
 };
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onRate}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
